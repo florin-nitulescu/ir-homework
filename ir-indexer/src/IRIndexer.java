4b152ab23bb4029436ebec378f236fc420158c8d
Nitzu <florin.nitulescu@compu-cons.ro>
2015-05-30 19:13:55 +0300

    Am extins demo-ul pentru a putea cauta cu diacritice si pentru a folosi o forma de sugesti

--- ir-indexer/src/IRIndexer.java
+++ ir-indexer/src/IRIndexer.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -14,39 +13,47 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.ro.RomanianAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.search.spell.Dictionary;
+import org.apache.lucene.search.spell.LuceneDictionary;
+import org.apache.lucene.search.spell.SpellChecker;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
 
 
 public class IRIndexer {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 
 		String docsPath = "E:\\ir-doc-collection";
 		final Path docDir = Paths.get(docsPath);
 		
 		final Path indexPath = docDir.getParent().resolve("ir-index-dir");
+		final Path spellIndexPath = docDir.getParent().resolve("ir-spell-index-dir");
 
 		if (!Files.isReadable(docDir)) {
 			System.out.println("Directorul ce contine colectia de documente (" + docDir.toAbsolutePath() + ") nu exista sau nu poate fi citit.");
 		}
 		
 		Date start = new Date();
+		Directory dir = null;
 		try {
 			System.out.println("Indexul va fi pastrat in directorul: " + indexPath.toAbsolutePath());
 			
-			Directory dir = FSDirectory.open(indexPath);
-			Analyzer analyzer = new StandardAnalyzer();
-			IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+			dir = FSDirectory.open(indexPath.toFile());
+			Analyzer analyzer = new RomanianAnalyzer();
+			IndexWriterConfig iwc = new IndexWriterConfig(Version.LATEST, analyzer);
 			
 			iwc.setOpenMode(OpenMode.CREATE);
 			
@@ -69,12 +76,31 @@ public class IRIndexer {
 			
 			writer.close();
 			
+			IndexReader indexReader = null;
+			Directory spellIndexDirectory = null;
+			try {
+				indexReader = IndexReader.open(dir);
+				spellIndexDirectory = FSDirectory.open(spellIndexPath.toFile());
+				
+				Dictionary dictionary = new LuceneDictionary(indexReader, "contents");
+				try (SpellChecker spellChecker = new SpellChecker(spellIndexDirectory)) {
+
+					IndexWriterConfig spellIwc = new IndexWriterConfig(Version.LATEST, analyzer);
+					spellChecker.indexDictionary(dictionary, spellIwc, true);
+				}
+			} finally {
+				if (indexReader != null) {
+					indexReader.close();
+				}
+			}
+			
 			Date end = new Date();
 			System.out.println("Total milisecunde: " + (end.getTime() - start.getTime()));
 						
 		} catch (IOException e) {
 			System.out.println("caught a " + e.getClass() + " \n with message: " + e.getMessage());
 		}
+		
 	
 		//System.out.println(docDir.getParent().toString());
 
---
+++ ir-search/src/IRSearch.java
@@ -0,0 +1,180 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.Vector;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.ro.RomanianAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.PhraseQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.spell.SpellChecker;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class IRSearch {
+	
+	static private class QuerySuggester extends QueryParser {
+		private boolean suggestedQuery = false;
+		private Directory spellIndexDirectory = null;
+		
+		public QuerySuggester(String field, Analyzer analyzer, Directory spellIndexDirectory) {
+			super(field, analyzer);
+			this.spellIndexDirectory = spellIndexDirectory;
+		}
+
+		@Override
+		protected Query getFieldQuery(String field, String queryText, boolean quoted)
+				throws ParseException {
+			// Copied from org.apache.lucene.queryParser.QueryParser
+			// replacing construction of TermQuery with call to getTermQuery()
+			// which finds close matches.
+			StringReader stringReader = new StringReader(queryText);
+		    TokenStream tokenStream;
+			Vector<String> v = new Vector<String>();
+			try {
+				tokenStream = getAnalyzer().tokenStream(field, stringReader);
+			    CharTermAttribute termAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+
+				String t;
+
+				tokenStream.reset();
+				
+				while (tokenStream.incrementToken()) {
+					t = termAttribute.toString();
+					if (t == null)
+						break;
+					v.addElement(t);
+				}
+				
+				tokenStream.close();
+			
+				System.out.println("Termenii din query: " + String.join(", ", v));
+	
+				if (v.size() == 0)
+					return null;
+				else if (v.size() == 1)
+					return new TermQuery(getTerm(field, v.elementAt(0)));
+				else {
+					PhraseQuery q = new PhraseQuery();
+					q.setSlop(getPhraseSlop());
+					for (int i = 0; i < v.size(); i++) {
+						q.add(getTerm(field, v.elementAt(i)));
+					}
+					return q;
+				}
+			} catch (IOException e1) {
+				return null;
+			}
+		}
+
+
+
+		private Term getTerm(String field, String queryText) throws ParseException, IOException {
+			SpellChecker spellChecker = new SpellChecker(spellIndexDirectory);
+			try {
+				if (spellChecker.exist(queryText)) {
+					return new Term(field, queryText);
+				}
+				String[] similarWords = spellChecker.suggestSimilar(queryText, 1);
+				if (similarWords.length == 0) {
+					return new Term(field, queryText);
+				}			
+				suggestedQuery = true;
+				return new Term(field, similarWords[0]);
+			} catch (IOException e) {
+				throw new ParseException(e.getMessage());
+			} finally {
+				spellChecker.close();
+			}
+		}		
+		
+		public boolean hasSuggestedQuery() {
+			return suggestedQuery;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		String indexPath = "E:\\ir-index-dir";
+		
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath).toFile()));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		Analyzer analyzer = new RomanianAnalyzer();
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+		
+		
+		Directory spellIndexDirectory = FSDirectory.open(Paths.get("E:\\ir-spell-index-dir").toFile());
+		QuerySuggester suggester = new QuerySuggester("contents", analyzer, spellIndexDirectory);
+		suggester.setDefaultOperator(QueryParser.AND_OPERATOR);
+		
+		while (true) {
+			System.out.print("Interogare: ");
+			String line = in.readLine();
+			
+			if (line == null || line.length() == -1) {
+				break;
+			}
+			
+			line = line.trim();
+			if (line.length() == 0) {
+				break;
+			}
+			
+
+			Query query = suggester.parse(line);
+			
+			if (suggester.hasSuggestedQuery()) {
+
+				System.out.println("Query cu sugestie!");
+			}
+			System.out.println("Se cauta dupa: " + query.toString("contents"));
+			
+			Date start = new Date();
+			TopDocs results = searcher.search(query, 100000);
+			Date end = new Date();
+			
+			System.out.println("Timp cautare: " + (end.getTime() - start.getTime()) + "ms");
+			
+			ScoreDoc[] hits = results.scoreDocs;
+			int numTotalHits = results.totalHits;
+			
+			
+			System.out.println("Nr. de rezultate returnate: " + numTotalHits);
+			
+			for (int i = 0; i < hits.length; i++) {
+				Document doc = searcher.doc(hits[i].doc);
+				String path = doc.get("path");
+				if (path != null) {
+					System.out.println((i + 1) + ". Scor: " + hits[i].score + "\n    Cale: " + path);
+					String title = doc.get("title");
+					if (title != null) {
+						System.out.println("  Titlu: " + title);
+					}
+				} else {
+					System.out.println((i + 1) + ". Scor: " + hits[i].score + "\n    Cale: Document fara cale");
+				}
+			}
+		}
+		
+		System.out.println("Gata.");
+		
+	}
+
+}
